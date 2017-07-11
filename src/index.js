7375473e819b435d1726436847b099ac628ac686
JaroInside <jaro0116@gmail.com>
2017-07-11 13:59:35 +0900

    add login form

--- src/index.js
+++ src/index.js
@@ -1,21 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+
+// Router
 import { Router, Route, browserHistory, IndexRoute } from 'react-router';
 
+// Container Components
 import { App, Home, Login, Register } from './containers';
+
+// Redux
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import reducers from './reducers';
+import thunk from 'redux-thunk';
+
 import registerServiceWorker from './registerServiceWorker';
+
+const store = createStore(reducers, applyMiddleware(thunk));
 
 const rootElement = document.getElementById('root');
 ReactDOM.render(
+  <Provider store={store}>
     <Router history={browserHistory}>
-        <Route path="/" component={App}>
-            <IndexRoute component={Home}/>
-            <Route path="home" component={Home}/>
-            <Route path="login" component={Login}/>
-            <Route path="register" component={Register}/>
-        </Route>
-    </Router>, rootElement
+      <Route path="/" component={App}>
+        <IndexRoute component={Home}/>
+        <Route path="home" component={Home}/>
+        <Route path="login" component={Login}/>
+        <Route path="register" component={Register}/>
+      </Route>
+    </Router>
+  </Provider>, rootElement
 );
 
 registerServiceWorker();
